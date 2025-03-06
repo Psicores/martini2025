c0a50b10d8a9bb7995acf60e3c89259d4a89e07c
franciscogigena91@gmail.com <franciscogigena91>
2025-03-06 13:57:34 -0300

    Menu unstiled

--- functions.php
+++ functions.php
@@ -4,13 +4,20 @@
  * Añado hoja de estilos
  */
 
- function martini_theme_scripts() {
-    wp_enqueue_style( 'style.css', get_stylesheet_uri() );
- }
- add_action("wp_enqueue_scripts","martini_theme_scripts");
-
- /* */
- function martini_theme_setup() {
-   add_theme_support('post-thumbnails'); //Imagen destacada
+function martini_theme_scripts()
+{
+   wp_enqueue_style('style.css', get_stylesheet_uri());
 }
-add_action("after_setup_theme","martini_theme_setup");
+add_action("wp_enqueue_scripts", "martini_theme_scripts");
+
+/* */
+function martini_theme_setup()
+{
+   add_theme_support('post-thumbnails'); //Imagen destacada
+
+   register_nav_menus(array(
+      'menu-1' => esc_html__('Header Menu', 'martini_theme'),
+      'menu-2' => esc_html__('Footer Menu', 'martini_theme'),
+   ));
+}
+add_action("after_setup_theme", "martini_theme_setup");
--- index.php
+++ index.php
@@ -8,6 +8,16 @@
 </head>
 <body>
 <?php wp_body_open() ?>
+
+<?php
+wp_nav_menu(
+    array(
+        'theme_location' => 'menu-1', // Ubicación del menú registrado con register_nav_menu
+        'menu_id' => 'Header-Menu',
+    )
+);
+?>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 <?php the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>'); ?>
