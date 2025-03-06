30afcf7623bb0d5157a9fe0afddaf39bef0ebf6e
franciscogigena91@gmail.com <franciscogigena91>
2025-03-06 09:51:16 -0300

    Add font Family Fashion Fetish

--- index.php
+++ index.php
@@ -4,17 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Martini Seguros</title>
-
-     <!-- Precargar fuentes -->
-    <!-- Precargar fuentes -->
-    <link rel="preload" href="<?php echo get_template_directory_uri(); ?>/assets/fonts/FashionFetish-Bold.woff2" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="<?php echo get_template_directory_uri(); ?>/assets/fonts/NevermindHeadline-Bold.woff2" as="font" type="font/woff2" crossorigin>
     <?php wp_head() ?>
 </head>
 <body>
     
-
-
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
     <?php the_title(); ?>
