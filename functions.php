a3d7583284ecd4118e8a61a23563ac171e1c11e5
franciscogigena91@gmail.com <franciscogigena91>
2025-03-06 13:33:39 -0300

    Title redirect to single

--- functions.php
+++ functions.php
@@ -8,3 +8,9 @@
     wp_enqueue_style( 'style.css', get_stylesheet_uri() );
  }
  add_action("wp_enqueue_scripts","martini_theme_scripts");
+
+ /* */
+ function martini_theme_setup() {
+   add_theme_support('post-thumbnails'); //Imagen destacada
+}
+add_action("after_setup_theme","martini_theme_setup");
--- index.php
+++ index.php
@@ -7,10 +7,10 @@
     <?php wp_head() ?>
 </head>
 <body>
-    
+<?php wp_body_open() ?>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-    <?php the_title(); ?>
+<?php the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>'); ?>
     <?php the_content(); ?>
     <?php the_post_thumbnail(); ?>
     <?php the_category(); ?>
