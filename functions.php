24ae5868e745fdedbda431298f31255d34c61a9c
Psicores <franciscogigena91@gmail.com>
2025-03-06 23:51:28 -0300

    add footer, header and custom logo

--- functions.php
+++ functions.php
@@ -15,6 +15,13 @@ function martini_theme_setup()
 {
    add_theme_support('post-thumbnails'); //Imagen destacada
 
+   add_theme_support('custom-logo', array(
+      'height' => 100, // Altura máxima del logotipo
+      'width' => 250, // Ancho máximo del logotipo
+      'flex-height' => true, // Permite que la altura sea flexible
+      'flex-width' => true, // Permite que el ancho sea flexible
+   ));
+
    register_nav_menus(array(
       'menu-1' => esc_html__('Header Menu', 'martini_theme'),
       'menu-2' => esc_html__('Footer Menu', 'martini_theme'),
--- index.php
+++ index.php
@@ -1,34 +1,14 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Martini Seguros</title>
-    <?php wp_head() ?>
-</head>
-<body>
-<?php wp_body_open() ?>
+<?php get_header(); ?>
+<?php if (have_posts()):
+    while (have_posts()):
+        the_post(); ?>
 
-<?php
-wp_nav_menu(
-    array(
-        'theme_location' => 'menu-1', // Ubicación del menú registrado con register_nav_menu
-        'menu_id' => 'Header-Menu',
-    )
-);
-?>
+        <?php the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>'); ?>
+        <?php the_content(); ?>
+        <?php the_post_thumbnail(); ?>
+        <?php the_category(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-<?php the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>'); ?>
-    <?php the_content(); ?>
-    <?php the_post_thumbnail(); ?>
-    <?php the_category(); ?>
-
-<?php endwhile; else : ?>
-	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endwhile; else: ?>
+    <p><?php esc_html_e('Sorry, no posts matched your criteria.'); ?></p>
 <?php endif; ?>
-
-    
-</body>
-</html>
+<? get_footer(); ?>
