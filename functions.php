9ced754538cb916f05d2f30da9fabe969ac74336
franciscogigena91@gmail.com <franciscogigena91>
2025-03-07 09:39:25 -0300

    Rename logo. Add function admin menu.

--- functions.php
+++ functions.php
@@ -15,16 +15,26 @@ function martini_theme_setup()
 {
    add_theme_support('post-thumbnails'); //Imagen destacada
 
-   add_theme_support('custom-logo', array(
-      'height' => 100, // Altura máxima del logotipo
-      'width' => 250, // Ancho máximo del logotipo
-      'flex-height' => true, // Permite que la altura sea flexible
-      'flex-width' => true, // Permite que el ancho sea flexible
-   ));
-
    register_nav_menus(array(
       'menu-1' => esc_html__('Header Menu', 'martini_theme'),
-      'menu-2' => esc_html__('Footer Menu', 'martini_theme'),
    ));
 }
 add_action("after_setup_theme", "martini_theme_setup");
+
+
+function martini_theme_add_admin_menu()
+{
+   // Obtener la URL del icono SVG
+   $icon_url = get_template_directory_uri() . '/assets/images/logo_admin.svg';
+
+   add_menu_page(
+      "Opciones de tema",
+      "Opciones de tema",
+      "manage_options",
+      "martini_theme_options_page",
+      'dashicons-admin-generic',
+      $icon_url,
+      60     
+   );
+}
+add_action('admin_menu', 'martini_theme_add_admin_menu');
